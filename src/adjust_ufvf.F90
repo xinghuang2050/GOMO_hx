f66d505cbe4de0a9d7acf81a7cb5a6afb30e8fe3
nsccwx-hx <huangxing275@126.com>
2019-04-04 15:34:59 +0800

    Create adjust_ufvf.F90

---
+++ src/adjust_ufvf.F90
@@ -0,0 +1,23 @@
+#include "macro.h"
+
+subroutine adjust_ufvf()
+  use openarray
+  use config
+  use variables
+  implicit none
+  integer::k,ierr
+  type(array)::fluc_u,fluc_v,sum_udz,sum_vdz
+
+  u=u+0.5d0*smoth*(uf+ub-2.d0*u-sum((uf+ub-2.d0*u) * dz, 3))
+  call set(sub(u,':',':',kb),0.d0)
+
+  v=v+ 0.5d0*smoth*(vf+vb-2.d0*v-sum((vf+vb-2.d0*v) * dz, 3))
+  call set(sub(v,':',':',kb),0.d0)
+
+  ub = u
+  u  = uf
+
+  vb = v
+  v  = vf
+
+end subroutine
